2e4c41711e2c6a5b6f2b2a0c635b37292c229735
CRprogrammer <oscarm.gonzalez@outlook.com>
2016-07-17 08:29:27 -0500

    Content/images, GuestIndex, BundleConfig,favicon

--- HotelesPuraVida/HotelesPuraVida/Controllers/HomeController.cs
+++ HotelesPuraVida/HotelesPuraVida/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace HotelesPuraVida.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult GuestIndex()
         {
             return View();
         }
